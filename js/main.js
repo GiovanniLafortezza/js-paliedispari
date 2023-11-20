089b091f736330be882a3c58f15393b39db44e0c
GiovanniLafortezza <recordeknot388@gmail.com>
2023-11-20 17:45:47 +0100

    Palindroma

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
 // --------------------- POLINDROMA --------------------------
 
 let word = prompt("Iserisci una parola");
+let isPalindroma = areStringEqual(word);
 
-
-
-
-
+    if (isPalindroma)
+        console.log("la parola è palindroma");
+    else
+        console.log("la parola non è palindroma");
 
 
 
 
 // ---------------------- FUNZIONI --------------------------------
 
-function reverseWord() {
+function getReverseWord(str) {
+    let strReverse = "";
     
+    for( let i = str.length - 1; i >= 0; i--){
+        
+         strReverse += str[i];
+    }
+    
+    return strReverse;
+
+}
+
+function areStringEqual (str){
+    let revertedStr = getReverseWord(str);
+    
+    if (revertedStr == str) return true;
+    else return false;
 }
