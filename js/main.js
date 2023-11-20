7629119c6ce33a81fcd2da2d25185c70411e2244
GiovanniLafortezza <recordeknot388@gmail.com>
2023-11-20 18:34:11 +0100

    odd or even

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// --------------------- POLINDROMA --------------------------
+// --------------------- PALINDROMA --------------------------
 
 let word = prompt("Iserisci una parola");
 let isPalindroma = areStringEqual(word);
@@ -9,10 +9,31 @@ let isPalindroma = areStringEqual(word);
         console.log("la parola non è palindroma");
 
 
+//------------------------- PARI O DISPARI ------------------------
 
+let selectOddOrEven = prompt("Scegli pari o dispari");
+console.log(selectOddOrEven);
 
+let inputNumberFrom1To5 = prompt("Inserire un numero da 1 a 5");
+console.log(inputNumberFrom1To5);
+inputNumberFrom1To5 = parseInt(inputNumberFrom1To5);
+
+let generateRandomNumberFrom1To5 = getRandomIntegerFromInterval(1, 5);
+console.log(generateRandomNumberFrom1To5);
+
+let sumOfTwoNumbers = sumTwoNumber(inputNumberFrom1To5, generateRandomNumberFrom1To5);
+console.log(sumOfTwoNumbers);
+
+let OddOrEven = isOddOrEven(sumOfTwoNumbers)
+
+if(OddOrEven) {
+    console.log("il numero è pari");
+} else {
+    console.log("il numero è dispari");
+}
 // ---------------------- FUNZIONI --------------------------------
 
+//------------- 
 function getReverseWord(str) {
     let strReverse = "";
     
@@ -31,3 +52,22 @@ function areStringEqual (str){
     if (revertedStr == str) return true;
     else return false;
 }
+
+function getRandomIntegerFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function sumTwoNumber(num1, num2) {
+    let result = num1 + num2;
+    return result;
+}
+
+function isOddOrEven(num) {
+    if(num % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
